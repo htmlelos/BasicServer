7c3bf1547ea3bdd65fb6e896238c6bba14a1182b
sergiolucero73 <sergiolucero73@gmail.com>
2018-04-28 11:24:51 -0300

    cambio a promesas

--- src/user/controller.js
+++ src/user/controller.js
@@ -2,96 +2,51 @@ const User = require('./model')
 
 function listUser() {
   return function (req, res) {
-    const {
-      fields
-    } = req.body
+    const {fields} = req.body
     User
       .find({})
       .select(fields)
-      .then(users => {
-        res.status(200).json({
-          success: true,
-          users
-        })
-      })
-      .catch(err => {
-        res.status(404).json({
-          success: false,
-          message: 'No se encontraron usuarios'
-        })
-      })
+      .then(users => {res.status(200).json({ success: true, users })})
+      .catch(err => { res.status(404).json({ success: false, message: 'No se encontraron usuarios' })})
   };
 }
 
 function getUser() {
   return function (req, res) {
-    const userId = req.params.userId;
+    const { userId } = req.params;
     User
       .findById(userId, 'name password email')
-      .then(user => {
-        res.status(200).json(user)
-      })
-      .catch(err => {
-        res.status(404).json({
-          succes: false,
-          message: 'Usuario no encontrado'
-        })
-      })
+      .then(user => {res.status(200).json(user)})
+      .catch(err => {res.status(404).json({success: false, message: 'Usuario no encontrado'})})
   };
 }
 
 function createUser() {
   return function (req, res) {
-    const newUser = req.body
-    const user = new User(newUser)
+    const user = new User(req.body)
     user
       .save()
-      .then(user => {
-        res.status(201).json({
-          success: true
-        });
-      })
-      .catch(err => {
-        res.status(401).json({
-          succes: false,
-          message: 'El usuario no pudo ser creado'
-        })
-      })
+      .then(user => {res.status(201).json({success: true});})
+      .catch(err => {res.status(404).json({success: false,message: 'El usuario no pudo ser creado'})})
   }
 }
 
 function updateUser() {
   return function (req, res) {
-    const userId = req.params.userId
+    const { userId } = req.params
     const user = req.body
-    User.findByIdAndUpdate(userId, user, {}, function (err, user) {
-      if (err) {
-        res.status(401).json({
-          success: false
-        })
-      }
-      res.status(200).json({
-        success: true
-      })
-    })
+    User.findByIdAndUpdate(userId, user, {})
+      .then(user => { res.status(200).json({success: true})})
+      .catch(err => {res.status(404).json({success: false, message: 'No se pudo actualizar el usuario'})})
   }
 }
 
 function deleteUser() {
   return function (req, res) {
-    const userId = req.params.userId;
-    User.remove({
-      _id: userId
-    }, function (err) {
-      if (err) {
-        res.status(401).json({
-          success: false
-        })
-      }
-      res.status(200).json({
-        success: true
-      })
-    });
+    const { userId } = req.params
+    User.findByIdAndRemove(userId)
+      .then(user => { res.status(200).json({success: true})})
+      .catch(err => {res.status(404).json({success: false})})
   }
 }
 
