6bb9dd13cc3bfa5489fb824d3495693dc14728de
Sergio Lucero <sergiolucero73@gmail.com>
2018-05-11 19:49:52 -0300

    change Promises to async-await

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,53 +1,66 @@
 const User = require('./model')
 
 const listUser = () => {
-  return (req, res) => {
+  return async (req, res) => {
     const {fields} = req.body
-    User
-      .find({})
-      .select(fields)
-      .then(users => {res.status(200).json({ success: true, users })})
-      .catch(err => { res.status(404).json({ success: false, message: 'No se encontraron usuarios' })})
+    try {
+      const users = await User.find({}).select(fields)
+      res.status(200).json({success: true, users})
+    } catch (error) {
+      res.status(404).json({success: false, message: 'No se encontraron usuario'})
+    }
+
   };
 }
 
 const getUser = () =>  {
-  return (req, res)  => {
+  return async (req, res)  => {
     const { userId } = req.params;
-    User
-      .findById(userId, 'name password email')
-      .then(user => {res.status(200).json(user)})
-      .catch(err => {res.status(404).json({success: false, message: 'Usuario no encontrado'})})
+    try {
+      const user = await User.findById(userId, 'name password email')
+      res.status(200).json(user)
+    } catch (error) {
+      res.status(404).json({success: false, message: 'Usuario no encontrado'})
+    }
   };
 }
 
 const createUser = () => {
-  return (req, res) => {
+  return async (req, res) => {
     const user = new User(req.body)
-    user
-      .save()
-      .then(user => {res.status(201).json({success: true});})
-      .catch(err => {res.status(404).json({success: false,message: 'El usuario no pudo ser creado'})})
+    try {
+      const newUser = await user.save()
+      res.status(201).json({success: true})
+    } catch (error) {
+      res.status(404).json({success: false,message: 'El usuario no pudo ser creado'})
+    }
+
   }
 }
 
 const updateUser = () => {
-  return (req, res) => {
-    console.log('BODY>>', req.body)
+  return async (req, res) => {
     const { userId } = req.params
     const user = req.body
-    User.findByIdAndUpdate(userId, user, {})
-      .then(user => { res.status(200).json({success: true})})
-      .catch(err => {res.status(404).json({success: false, message: 'No se pudo actualizar el usuario'})})
+    try {
+      const newUser = await User.findByIdAndUpdate(userId, user, {})
+      res.status(200).json({success: true})
+    } catch (error) {
+      res.status(404).json({success: false, message: 'No se pudo actualizar el usuario'})
+    }
   }
 }
 
 const deleteUser = () => {
-  return (req, res) => {
+  return async (req, res) => {
     const { userId } = req.params
-    User.findByIdAndRemove(userId)
-      .then(user => { res.status(200).json({success: true})})
-      .catch(err => {res.status(404).json({success: false})})
+    try {
+      const user = await User.findByIdAndRemove(userId)
+      res.status(200).json({success: true})
+    } catch (error) {
+      res.status(404).json({success: false})
+    }
+
   }
 }
 
